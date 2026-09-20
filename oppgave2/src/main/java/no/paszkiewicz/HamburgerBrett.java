314343cc83d8bbf0b2293ef32c8aeb63795f167d
Bartosz Paszkiewicz <bartosz@paszkiewicz.no>
2026-09-20 11:44:39 +0200

    Endret HamburgerBrett

--- oppgave2/src/main/java/no/paszkiewicz/HamburgerBrett.java
+++ oppgave2/src/main/java/no/paszkiewicz/HamburgerBrett.java
@@ -1,27 +1,46 @@
 package no.paszkiewicz;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class HamburgerBrett {
     private final int kapasitet;
-    private Queue<Hamburger> brett = new LinkedList<>();
     
+    private Hamburger[] brett;
+
+    private int antallKonsumert = 0;
+    private int antallProdusert = 0;
+    private int antall = 0;
+
 
     public HamburgerBrett(int kapasitet){
         this.kapasitet = kapasitet;
+        brett = new Hamburger[kapasitet];
     }
 
     public synchronized void addBurger(Hamburger burger) throws InterruptedException {
-        while (brett.size() >= kapasitet) {
+        while (antall == kapasitet) {
             wait();
         }
         
-        brett.add(burger);        
+        brett[antallProdusert % kapasitet] = burger;
+        antallProdusert++;
+        antall++;
+
+        notifyAll();
     }
 
-    public synchronized Hamburger removeBurger() {
+    public synchronized Hamburger removeBurger() throws InterruptedException{
+        while (antall == 0) {
+            System.out.println("No borgir for you!");
+            wait();
+        }
 
-        return brett.poll();
+        Hamburger burger = brett[antallKonsumert % kapasitet];
+        brett[antallKonsumert % kapasitet] = null;
+
+        antallKonsumert++;
+        antall--;
+
+        notifyAll();
+
+        return burger;
     }
 }
